b49ed4d62a3fbe469fca4b2fafe5f1eb80d37541
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-08-27 19:58:19 +0200

    added script cleaning and storing all pems on init
    Signed-off-by: Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>

--- puppet/manifests/service.pp
+++ puppet/manifests/service.pp
@@ -1,5 +1,24 @@
 class puppetizer_main::service {
+
+  $store_script = '/usr/local/bin/letsencrypt-store'
+  file { $store_script:
+    mode => 'a=rx,u+w',
+    content => epp('puppetizer_main/consul.py.epp', {
+      'letsencrypt_key' => $puppetizer_main::consul_key,
+      'letsencrypt_etc' => $puppetizer_main::config_dir,
+      'consul_token'    => $puppetizer_main::consul_token,
+      'consul_host'     => $puppetizer_main::consul_host,
+      'consul_port'     => $puppetizer_main::consul_port,
+      'consul_scheme'   => $puppetizer_main::consul_scheme
+    })
+  }
+
+  exec { 'letsencrypt-store':
+    command => $store_script,
+    noop    => $facts['puppetizer']['building']
+  }
   puppetizer::service { 'cron':
     run_content => "#!/bin/sh -e\nexec crond -f -d 7",
+    require => Exec['letsencrypt-store']
   }
 }
